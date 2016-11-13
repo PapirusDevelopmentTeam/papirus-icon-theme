249d59408da06cc589ad9b44ea96b162df8f9196
Alexey Varfolomeev <varlesh@gmail.com>
2016-11-14 01:16:46 +0300

    indicator-kdeconnect tray icons

--- install-papirus-home.sh
+++ install-papirus-home.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/usr/bin/env bash
 echo "Papirus icon theme for GTK"
 sleep 5
 echo "Delete old Papirus icon theme ..."
--- remove-papirus.sh
+++ remove-papirus.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/usr/bin/env bash
 echo "Remove Papirus icon theme for GTK"
 sudo rm -rf /usr/share/icons/{Papirus-GTK,Papirus-Dark-GTK}
 rm -rf ~/.icons/{Papirus-GTK,Papirus-Dark-GTK}
