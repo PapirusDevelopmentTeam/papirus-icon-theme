8afd113e04bfac58b5f530f4f14850819f08a99c
varlesh <varlesh@gmail.com>
2016-11-16 01:24:20 +0300

    Fix install scripts on Fedorino Gore :) #148

--- install-papirus-root.sh
+++ install-papirus-root.sh
@@ -6,7 +6,7 @@ sudo rm -rf /usr/share/icons/{Papirus-GTK,Papirus-Dark-GTK}
 echo "Download new version from GitHub ..."
 wget -c https://github.com/PapirusDevelopmentTeam/papirus-icon-theme-gtk/archive/master.zip -O /tmp/papirus-icon-theme-gtk.zip
 echo "Unpack archive ..."
-7z x /tmp/papirus-icon-theme-gtk.zip -o/tmp/
+7za x /tmp/papirus-icon-theme-gtk.zip -o/tmp/
 echo "Installing ..."
 sudo cp -R /tmp/papirus-icon-theme-gtk-master/{Papirus-GTK,Papirus-Dark-GTK} /usr/share/icons/
 sudo chmod -R 755 /usr/share/icons/{Papirus-GTK,Papirus-Dark-GTK}
