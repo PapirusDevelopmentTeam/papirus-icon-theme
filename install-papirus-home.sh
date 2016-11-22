74fd5ed1d76245e59bf0d4d6c75cf147181ea8fb
Mikael Göransson <github@mgor.se>
2016-11-22 07:46:13 +0100

    Replace wget with curl
    Realized that wget --show-progress parameter is not available
    in the version, 1.15, provided in Ubuntu 14.04 LTS. Changed
    to curl instead.

--- install-papirus-home.sh
+++ install-papirus-home.sh
@@ -2,11 +2,12 @@
 
 echo "Papirus icon theme for GTK"
 ! which 7za > /dev/null 2>&1 && { echo "Please install p7zip"; exit 1; }
+! which curl > /dev/null 2>&1 && { echo "Please install curl"; exit 1; }
 echo "Delete old Papirus icon theme ..."
 rm -rf ~/.icons/{Papirus-GTK,Papirus-Dark-GTK}
 echo "Download new version from GitHub ..."
-wget -q --show-progress -c https://github.com/PapirusDevelopmentTeam/papirus-icon-theme-gtk/archive/master.zip \
-    -O /tmp/papirus-icon-theme-gtk.zip
+curl -o /tmp/papirus-icon-theme-gtk.zip -L \
+    https://github.com/PapirusDevelopmentTeam/papirus-icon-theme-gtk/archive/master.zip
 echo "Unpack archive ..."
 7za x /tmp/papirus-icon-theme-gtk.zip -o/tmp/ > /dev/null
 echo "Installing ..."
--- install-papirus-root.sh
+++ install-papirus-root.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 echo "Papirus icon theme for GTK"
 ! which 7za > /dev/null 2>&1 && { echo "Please install p7zip"; exit 1; }
+! which curl > /dev/null 2>&1 && { echo "Please install curl"; exit 1; }
 echo "Delete old Papirus icon theme ..."
 sudo rm -rf /usr/share/icons/{Papirus-GTK,Papirus-Dark-GTK}
 echo "Download new version from GitHub ..."
-wget -q --show-progress -c https://github.com/PapirusDevelopmentTeam/papirus-icon-theme-gtk/archive/master.zip \
-    -O /tmp/papirus-icon-theme-gtk.zip
+curl -o /tmp/papirus-icon-theme-gtk.zip -L \
+    https://github.com/PapirusDevelopmentTeam/papirus-icon-theme-gtk/archive/master.zip
 echo "Unpack archive ..."
 7za x /tmp/papirus-icon-theme-gtk.zip -o/tmp/ > /dev/null
 echo "Installing ..."
