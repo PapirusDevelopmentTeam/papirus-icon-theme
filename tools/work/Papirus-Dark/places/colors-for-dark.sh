4ca3c1cbc6347b16dc5a0fed483a54672ab462f2
varlesh <varlesh@gmail.com>
2016-12-08 14:50:41 +0400

    rename template & moved scripts to tools

--- tools/work/Papirus-Dark/places/colors-for-dark.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script change Papirus colors to Papirus Dark
-# USAGE:
-#./colors-dark.svg
-echo "Change colors for Papirus Dark"
-echo "Press <CTRL-C> to abort (wait 5 seconds) ..."
-sleep 5
-sed -i 's|{ color:#5c616c; }|{ color:#d3dae3; }|g' *.svg
-sed -i 's|id="papirus"|id="papirus-dark"|g' *.svg
