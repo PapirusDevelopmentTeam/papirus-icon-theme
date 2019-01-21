e559a22def7f5f5b4ed2c936d2769b7637f37528
varlesh <varlesh@gmail.com>
2019-01-22 00:54:43 +0400

    Delete 1.sh

--- Papirus-Light/24x24/1.sh
+++
@@ -1,9 +0,0 @@
-ln -s ../../Papirus/24x24/emotes
-ln -s ../../Papirus/24x24/status
-ln -s ../../Papirus/24x24/devices
-ln -s ../../Papirus/24x24/actions
-ln -s ../../Papirus/24x24/mimetypes
-ln -s ../../Papirus/24x24/places
-ln -s ../../Papirus/24x24/emblems
-ln -s ../../Papirus/24x24/apps
-ln -s apps categories
