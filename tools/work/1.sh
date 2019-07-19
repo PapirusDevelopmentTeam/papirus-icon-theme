6cdcef22463d5bc9bb737fa3a63a07ac8f974336
Alexey Varfolomeev <varlesh@gmail.com>
2019-07-19 09:23:04 +0400

    symlinks LibreOffice 6.2 & add Chart icon

---
+++ tools/work/1.sh
@@ -0,0 +1,9 @@
+./new-symlink.sh apps libreoffice-base.svg libreoffice6.2-base.svg
+./new-symlink.sh apps libreoffice-calc.svg libreoffice6.2-calc.svg
+./new-symlink.sh apps libreoffice-chart.svg libreoffice6.2-chart.svg libreoffice6.0-chart.svg libreofficedev6.0-chart.svg libreofficedev6.1-chart.svg org.libreoffice.LibreOffice.chart.svg
+./new-symlink.sh apps libreoffice-draw.svg libreoffice6.2-draw.svg
+./new-symlink.sh apps libreoffice-impress.svg libreoffice6.2-impress.svg
+./new-symlink.sh apps libreoffice-main.svg libreoffice6.2-main.svg libreoffice6.2-startcenter.svg
+./new-symlink.sh apps libreoffice-math.svg libreoffice6.2-math.svg
+./new-symlink.sh apps libreoffice-writer.svg libreoffice6.2-writer.svg
+
