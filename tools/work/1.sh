d6db7f73ce9dad9a2d796c7ffb04aa2fe08b781f
Alexey Varfolomeev <varlesh@gmail.com>
2019-08-02 12:25:24 +0400

    some TDE symlinks #1818

---
+++ tools/work/1.sh
@@ -0,0 +1,6 @@
+./new-symlink.sh actions view-more.svg view_choose.svg
+./new-symlink.sh actions view-list-details.svg view_detailed.svg
+./new-symlink.sh actions view-grid.svg view_icon.svg
+./new-symlink.sh actions view-column.svg view_multicolumn.svg
+./new-symlink.sh actions view-list-text.svg view_text.svg
+./new-symlink.sh actions view-filter-rtl.svg view_tree.svg
