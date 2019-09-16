df20f994e1c1ef58facc46740b96405483fbde46
Alexey Varfolomeev <varlesh@gmail.com>
2019-09-16 21:59:58 +0400

    Delete 1.sh

--- tools/work/1.sh
+++
@@ -1,4 +0,0 @@
-./new-symlink.sh panel network-wireless-signal-low.svg signal-25.svg
-./new-symlink.sh panel network-wireless-signal-low.svg signal-50.svg
-./new-symlink.sh panel network-wireless-signal-good.svg signal-75.svg
-./new-symlink.sh panel network-wireless-signal-excellent.svg signal-100.svg
