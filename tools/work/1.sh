6dec59b7ec4de65faa1cf6e50a24542baae539ed
Alexey Varfolomeev <varlesh@gmail.com>
2020-06-15 15:30:07 +0400

    Delete 1.sh

--- tools/work/1.sh
+++
@@ -1,5 +0,0 @@
-./new-symlink.sh actions view-compact.svg fm-compact.svg
-./new-symlink.sh actions view-list-details.svg fm-details.svg
-./new-symlink.sh actions view-grid.svg fm-icons.svg
-./new-symlink.sh actions folder-new.svg fm-new.svg
-./new-symlink.sh actions view-preview.svg fm-thumbs.svg
