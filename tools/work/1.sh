a10d1f4eab976affe6e7248c5a01867820c45447
Alexey Varfolomeev <varlesh@gmail.com>
2021-08-11 10:01:30 +0400

    eOS symlinks feedback/music/photos/sideload/videos/wingpanel

---
+++ tools/work/1.sh
@@ -0,0 +1,6 @@
+./new-symlink.sh apps multimedia-photo-viewer.svg io.elementary.photos.svg
+./new-symlink.sh apps deepin-feedback.svg io.elementary.feedback.svg
+./new-symlink.sh apps multimedia-audio-player.svg io.elementary.music.svg 
+./new-symlink.sh apps ../mimetypes/application-vnd.flatpak.svg io.elementary.sideload.svg
+./new-symlink.sh apps multimedia-video-player.svg io.elementary.videos.svg
+./new-symlink.sh apps wingpanel.svg io.elementary.wingpanel.svg
