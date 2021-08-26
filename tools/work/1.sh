d13d2ddb8560c0498979db5111f2f432208c3539
Alexey Varfolomeev <varlesh@gmail.com>
2021-08-26 10:03:59 +0400

    fix links

---
+++ tools/work/1.sh
@@ -0,0 +1,26 @@
+./new-symlink.sh apps cups.svg dde-printer.svg
+./new-symlink.sh apps multimedia-photo-viewer.svg deepin-album.svg
+./new-symlink.sh apps software-store.svg deepin-app-store.svg
+./new-symlink.sh apps kphotoalbum.svg deepin-camera.svg
+./new-symlink.sh apps ark.svg deepin-compressor.svg
+./new-symlink.sh apps jockey.svg deepin-devicemanager.svg
+./new-symlink.sh apps disk-utility.svg deepin-diskmanager.svg
+./new-symlink.sh apps preferences-desktop-font.svg deepin-font-manager.svg
+./new-symlink.sh apps gnome-logs.svg deepin-log-viewer.svg
+./new-symlink.sh apps internet-mail.svg deepin-mail.svg
+./new-symlink.sh apps document-viewer.svg deepin-reader.svg
+./new-symlink.sh apps gnome-sound-recorder.svg deepin-voice-note.svg
+./new-symlink.sh apps mintsources-additional.svg downloader.svg
+./new-symlink.sh apps gnome-contacts.svg deepin-contacts.svg org.deepin.contacts.svg
+./new-symlink.sh apps internet-web-browser.svg deepin-browser.svg org.deepin.browser.svg uosbrowser-cn.svg
+./new-symlink.sh apps gufw.svg deepin-defender.svg
+./new-symlink.sh apps wine.svg deepin-wine-assist.svg
+./new-symlink.sh apps preferences-desktop-keyboard.svg fcitx-setting.svg
+./new-symlink.sh apps isomaster.svg iso-customizer.svg
+./new-symlink.sh apps distributor-logo-android.svg kbox.svg
+./new-symlink.sh apps com.bixense.PasswordCalculator.svg licensequick.svg
+./new-symlink.sh apps downloader-arrow.svg uos-downloadmanager.svg
+./new-symlink.sh apps ubiquity-kde.svg uos-installtool.svg
+./new-symlink.sh apps preferences-desktop-remote-desktop.svg uos-remote-assistance.svg
+./new-symlink.sh apps system-help.svg uos-service-support.svg
+./new-symlink.sh apps distributor-logo-windows.svg uos-windesk.svg
