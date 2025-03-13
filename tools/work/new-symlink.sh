2a3c8b2395ec03193221fc097ba592000012ff9b
Serhii Yeremenko <finalitik@gmail.com>
2025-03-13 07:48:06 +0200

    Update scripts to support the new location of symbolic icons

--- tools/work/new-symlink.sh
+++ tools/work/new-symlink.sh
@@ -16,7 +16,9 @@ usage() {
 
 	  available contexts:
 	    [ac]tions
+	    [an]imations
 	    [ap]ps
+	    [ca]tegories
 	    [d]evices
 	    [emb]lems
 	    [emo]tes
@@ -26,96 +28,111 @@ usage() {
 	    [s]tatus
 
 	Examples:
-	  $0 apps radiotray.svg radiotray-ng-on.svg
-	  $0 panel radiotray_off.svg radiotray-ng-off-panel.svg
-	  $0 panel radiotray_on.svg radiotray-ng-on-panel.svg
+	  $0 apps steam.svg com.valvesoftware.Steam.svg
+	  $0 panel indicator-cpufreq.svg cpu-frequency-indicator.svg
+	  $0 status ../devices/input-touchpad-symbolic.svg touchpad-enabled-symbolic.svg
 	EOF
 
 	exit 2
 }
 
-_get_icon_name() {
+has_symbolic_suffix() {
+	local icon_name="$1"
+
+	case "$icon_name" in
+		*-symbolic.svg|*-symbolic)
+			return 0
+			;;
+		*)
+			return 1
+			;;
+	esac
+}
+
+remove_file_extention() {
 	local icon_name="$1"
 
 	case "$icon_name" in
 		*.svg|*.png|*.xpm)
 			echo "${icon_name%.*}"
-			return 0
 			;;
 		*)
 			echo "${icon_name}"
-			return 0
 			;;
 	esac
-
-	return 1
 }
 
-_get_context() {
-	local CONTEXT
-	local -a SIZES
-
-	case "$1" in
-		actions|ac*)
-			CONTEXT="actions"
-			SIZES=( '16x16' '22x22' '24x24' )
-			;;
-		apps|ap*)
-			CONTEXT="apps"
-			SIZES=( '16x16' '22x22' '24x24' '32x32' '48x48' '64x64' )
-			;;
-		devices|d*)
-			CONTEXT="devices"
-			SIZES=( '16x16' '22x22' '24x24' '32x32' '48x48' '64x64' )
-			;;
-		emblems|emb*)
-			CONTEXT="emblems"
-			SIZES=( '16x16' '22x22' '24x24' '32x32' '48x48' )
-			;;
-		emotes|emo*)
-			CONTEXT="emotes"
-			SIZES=( '16x16' '22x22' '24x24' '32x32' '48x48' )
-			;;
-		mimetypes|m*)
-			CONTEXT="mimetypes"
-			SIZES=( '16x16' '22x22' '24x24' '32x32' '48x48' '64x64' )
-			;;
-		panel|pa*)
-			CONTEXT="panel"
-			SIZES=( '16x16' '22x22' '24x24' )
-			;;
-		places|pl*)
-			CONTEXT="places"
-			SIZES=( '16x16' '22x22' '24x24' '32x32' '48x48' '64x64' )
-			;;
-		status|s*)
-			CONTEXT="status"
-			SIZES=( '22x22' '24x24' '32x32' '48x48' )
-			;;
-		*)
-			printf "illegal context -- '%s'\n" "$1" >&2
-			printf false
-			return 1
-			;;
-	esac
-
-	declare -p CONTEXT SIZES
-}
-
-readonly RAW_CONTEXT="$1"
+readonly _CONTEXT="$1"
 readonly TARGET_ICON="$2"
-declare -a ARGS=("${@:3}")
 
-[ "${#ARGS[@]}" -gt 0 ] || usage
+[ "$#" -ge 3 ] || usage
 
-eval "$(_get_context "$RAW_CONTEXT")" || usage
+shift 2
 
-for i in "${ARGS[@]}"; do
-	symlink_name="$(_get_icon_name "$i")"
+CONTEXT_DIR=""
+SIZES=()
+
+case "$_CONTEXT" in
+	actions|ac*)
+		CONTEXT_DIR="actions"
+		SIZES=( '16x16' '22x22' '24x24' )
+		;;
+	animations|an*)
+		CONTEXT_DIR="animations"
+		SIZES=( '22x22' '24x24' )
+		;;
+	apps|ap*)
+		CONTEXT_DIR="apps"
+		SIZES=( '16x16' '22x22' '24x24' '32x32' '48x48' '64x64' )
+		;;
+	categories|ca*)
+		CONTEXT_DIR="categories"
+		SIZES=( '16x16' '22x22' '24x24' '32x32' '48x48' '64x64' )
+		;;
+	devices|d*)
+		CONTEXT_DIR="devices"
+		SIZES=( '16x16' '22x22' '24x24' '32x32' '48x48' '64x64' )
+		;;
+	emblems|emb*)
+		CONTEXT_DIR="emblems"
+		SIZES=( '16x16' '22x22' '24x24' '32x32' '48x48' )
+		;;
+	emotes|emo*)
+		CONTEXT_DIR="emotes"
+		SIZES=( '16x16' '22x22' '24x24' '32x32' '48x48' )
+		;;
+	mimetypes|m*)
+		CONTEXT_DIR="mimetypes"
+		SIZES=( '16x16' '22x22' '24x24' '32x32' '48x48' '64x64' )
+		;;
+	panel|pa*)
+		CONTEXT_DIR="panel"
+		SIZES=( '16x16' '22x22' '24x24' )
+		;;
+	places|pl*)
+		CONTEXT_DIR="places"
+		SIZES=( '16x16' '22x22' '24x24' '32x32' '48x48' '64x64' )
+		;;
+	status|s*)
+		CONTEXT_DIR="status"
+		SIZES=( '22x22' '24x24' '32x32' '48x48' )
+		;;
+	*)
+		printf "illegal context -- '%s'\n" "$1" >&2
+		usage
+		;;
+esac
+
+for symbolic in "$@"; do
+	symlink_name="$(remove_file_extention "$symbolic")"
+
+	if has_symbolic_suffix "$symlink_name"; then
+		SIZES=( '16' '22' '24' )
+	fi
 
 	for size in "${SIZES[@]}"; do
 		ln -sfv "$TARGET_ICON" \
-			"${TARGET_DIR}/${CONTEXT}/${symlink_name}@${size}.svg"
+			"$TARGET_DIR/$CONTEXT_DIR/${symlink_name}@${size}.svg"
 	done
 done
 
--- tools/work/put-into-theme.sh
+++ tools/work/put-into-theme.sh
@@ -7,6 +7,13 @@ set -eo pipefail
 readonly SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 readonly TARGET_DIR="$SCRIPT_DIR/../.."
 
+has_symbolic_suffix() {
+	case "$1" in
+		*-symbolic) return 0 ;;
+		*)          return 1 ;;
+	esac
+}
+
 mapfile -t SOURCE_DIRS < <(
 	find "$TARGET_DIR" -type f -name 'index.theme' -printf '%h\n' |
 		while read -r target_dir; do
@@ -16,15 +23,18 @@ mapfile -t SOURCE_DIRS < <(
 )
 
 find "${SOURCE_DIRS[@]}" -name '*.svg' | while read -r file; do
-	src_dir=$(dirname "$file")
-	top_dir=$(dirname "$src_dir")
-	base_name=$(basename "$file" .svg)
+	# Extract theme_dir, context_dir, icon name, and size_dir
+	if [[ $file =~ .*/([^/]+)/([^/]+)/(.+)@([0-9x]+)\.svg ]]; then
+		theme_dir="${BASH_REMATCH[1]}"
+		context_dir="${BASH_REMATCH[2]}"
+		icon_name="${BASH_REMATCH[3]}"
+		size_dir="${BASH_REMATCH[4]}"
+	fi
 
-	base_dir=$(basename "$top_dir")
-	context=$(basename "$src_dir")
-	filename="${base_name%%@*}"
-	size="${base_name##*@}"
+	if has_symbolic_suffix "$icon_name"; then
+		context_dir="symbolic/$context_dir"
+	fi
 
 	cp --no-preserve=mode,ownership --remove-destination -P -v "$file" \
-		"$TARGET_DIR/$base_dir/$size/$context/$filename.svg"
+		"$TARGET_DIR/$theme_dir/$size_dir/$context_dir/$icon_name.svg"
 done
