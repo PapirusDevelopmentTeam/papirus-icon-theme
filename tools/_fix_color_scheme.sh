b2e2de193586704bb45b7b69545844b1f5b35f42
Serhii Yeremenko <finalitik@gmail.com>
2025-03-15 10:26:19 +0200

    Add GNOME symbolic classes to all icons with KDE Color Scheme

--- tools/_fix_color_scheme.sh
+++ tools/_fix_color_scheme.sh
@@ -24,30 +24,18 @@
 set -e
 
 # Papirus, Papirus-Dark, Papirus-Light, and ePapirus
-add_class() {
+add_css_classes() {
 	# 1. remove class="ColorScheme-*" if currentColor is missing
 	# 2. remove class="ColorScheme-*" if color property is set
-	# 3. add a class if color value matches
+	# 3-7. add KDE ColorScheme-* and GNOME symbolic classes if color value matches
 	sed -i -r \
 		-e '/:currentColor/! s/[ ]class="ColorScheme-[^"]+"//g' \
 		-e '/[^-]color:[^;"]/ s/[ ]class="ColorScheme-[^"]+"//g' \
 		-e '/([^-]color|fill|stop-color|stroke):(#444444|#dfdfdf|#6e6e6e|#ffffff)/I s/(style="[^"]+")/\1 class="ColorScheme-Text"/' \
 		-e '/([^-]color|fill|stop-color|stroke):#4285f4/I s/(style="[^"]+")/\1 class="ColorScheme-Highlight"/' \
-		-e '/([^-]color|fill|stop-color|stroke):#4caf50/I s/(style="[^"]+")/\1 class="ColorScheme-PositiveText"/' \
-		-e '/([^-]color|fill|stop-color|stroke):#ff9800/I s/(style="[^"]+")/\1 class="ColorScheme-NeutralText"/' \
-		-e '/([^-]color|fill|stop-color|stroke):#f44336/I s/(style="[^"]+")/\1 class="ColorScheme-NegativeText"/' \
-		"$@"
-}
-
-# Symbolic
-add_class_symbolic() {
-	# 1. remove all success, warning, and error classes if exist
-	# 2. add classes if a color matches and a class is missing
-	sed -i -r \
-		-e '/class=/  { s/[ ]class="(success|warning|error)"//g }' \
-		-e '/class=/! { /([^-]color|fill|stop-color|stroke):#4caf50/I s/(style="[^"]+")/\1 class="success"/ }' \
-		-e '/class=/! { /([^-]color|fill|stop-color|stroke):#ff9800/I s/(style="[^"]+")/\1 class="warning"/ }' \
-		-e '/class=/! { /([^-]color|fill|stop-color|stroke):#f44336/I s/(style="[^"]+")/\1 class="error"/ }' \
+		-e '/([^-]color|fill|stop-color|stroke):#4caf50/I s/(style="[^"]+")/\1 class="ColorScheme-PositiveText success"/' \
+		-e '/([^-]color|fill|stop-color|stroke):#ff9800/I s/(style="[^"]+")/\1 class="ColorScheme-NeutralText warning"/' \
+		-e '/([^-]color|fill|stop-color|stroke):#f44336/I s/(style="[^"]+")/\1 class="ColorScheme-NegativeText error"/' \
 		"$@"
 }
 
@@ -72,21 +60,11 @@ for file in "$@"; do
 
 		if grep -q -i 'color:\(#444444\|#dfdfdf\|#6e6e6e\|#ffffff\)' "$file"; then
 			# it's Papirus, Papirus-Dark, Papirus-Light or ePapirus
-			add_class "$file"
+			add_css_classes "$file"
 		else
 			echo "'$file' has unknown colors!" >&2
 		fi
 
 		replace_hex_to_current_color "$file"
-	else
-		case "$file" in
-			*-symbolic.svg|*-symbolic-rtl.svg|*-symbolic@symbolic.svg|*-symbolic-rtl@symbolic.svg)
-				# it's symbolic icon
-				add_class_symbolic "$file"
-				;;
-			*)
-				continue
-				;;
-		esac
 	fi
 done
